c788257707a1e7ceb97d2050218c118a673dcd84
wphu <wphu@qq.com>
2018-11-01 16:39:40 +0800

    update

--- tools/third-part-packages-installer/codes-installer-user.sh
+++ tools/third-part-packages-installer/codes-installer-user.sh
@@ -1,15 +1,17 @@
-install_path_header=/home/huwanpeng/opt
-compiler_c=gcc
-compiler_cxx=g++
-compiler_fortran=gfortran
+export install_path_header=/home/huwanpeng/opt
+export compiler_c=gcc
+export compiler_cxx=g++
+export compiler_fortran=gfortran
+export compiler_mpicc=mpicc
+export compiler_mpicxx=mpicxx
+export source_codes_root_path=$(pwd)
 
 # install mpich3
-CC=compiler_c
+CC=${compiler_c}
 package=mpich-3.2.1
 install_path=mpich
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
@@ -19,20 +21,18 @@ else
     make
     make install
     cd ..
-    #rm -rf ${packages[i]}
 fi
 export PATH=${install_path_header}/${install_path}/bin:$PATH
-which mpicc
+
 
 
 # install hdf5
-CC=compiler_c
-CFLAGS=-fPIC
+CC=${compiler_c}
+export CFLAGS=-fPIC
 package=hdf5-1.8.20
 install_path=hdf5
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
@@ -43,15 +43,14 @@ else
     make install
     cd ..
 fi
-
+export CFLAGS=""
 
 # install hdf5-mpich
-CC=mpicc
+CC=${compiler_mpicc}
 package=hdf5-1.8.20
 install_path=hdf5-mpich
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
@@ -66,12 +65,11 @@ fi
 
 
 # install fftw
-CC=gcc
+CC=${compiler_c}
 package=fftw-3.3.4
 install_path=fftw
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
@@ -85,14 +83,13 @@ fi
 
 
 # install netcdf(netcdf-c)
-CC=gcc
+CC=${compiler_c}
 export CPPFLAGS="-I${install_path_header}/hdf5/include"
 export LDFLAGS="-L${install_path_header}/hdf5/lib"
 package=netcdf-4.6.0
 install_path=netcdf
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
@@ -108,8 +105,8 @@ export CPPFLAGS=""
 export LDFLAGS=""
 
 # install netcdf-cxx
-CC=gcc
-CXX=g++
+CC=${compiler_c}
+CXX=${compiler_cxx}
 export CPPFLAGS="-I${install_path_header}/hdf5/include -I${install_path_header}/netcdf/include"
 export LDFLAGS="-L${install_path_header}/hdf5/lib -L${install_path_header}/netcdf/lib"
 package=netcdf-cxx4-4.3.0
@@ -128,77 +125,123 @@ fi
 export CPPFLAGS=""
 export LDFLAGS=""
 
-# install SuperLU-4.3
-CC=compiler_c
-FC=compiler_fortran
-package=superlu_4.3
-install_path=SuperLU
+# install netcdf-cxx
+export CC=${compiler_c}
+export CPPFLAGS="-I${install_path_header}/hdf5/include -I${install_path_header}/netcdf/include"
+export LDFLAGS="-L${install_path_header}/hdf5/lib -L${install_path_header}/netcdf/lib"
+package=netcdf-cxx4-4.3.0
+install_path=netcdf
 if [ -d ${install_path_header}/${install_path} ];then
-    echo "${package} has been installed"
-    continue
-else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
     tar -xvf ${package}.gz
-    mv SuperLU_4.3 ${package}
     cd ${package}
     ./configure --prefix=${install_path_header}/${install_path}
     make
     make install
     cd ..
-    chmod a+r -R ${install_path_header}/${install_path}
+    #rm -rf ${package}
+fi
+export CPPFLAGS=""
+export LDFLAGS=""
+
+# install lapack
+FC=${compiler_fortran}
+export FFLAGS=-fPIC
+package=lapack-3.8.0
+install_path=lapack
+if [ -d ${install_path_header}/${install_path} ];then
+    echo "${package} has been installed"
+else
+    tar -xvf ${package}.tar.gz
+    tar -xvf ${package}.tar
+    tar -xvf ${package}.gz
+    cd ${package}
+    mkdir build
+    cd build
+    mkdir ${install_path_header}/${install_path}
+    cmake ../ -DCMAKE_INSTALL_PREFIX=${install_path_header}/${install_path}
+    make
+    make install
+    cd ../..
+fi
+export FFLAGS=""
+
+# install SuperLU
+export CC=${compiler_c}
+package=superlu_5.2.1
+install_path=superlu
+if [ -d ${install_path_header}/${install_path} ];then
+    echo "${package} has been installed"
+else
+    tar -xvf ${package}.tar.gz
+    tar -xvf ${package}.tar
+    tar -xvf ${package}.gz
+    mv SuperLU_5.2.1 ${package}
+    cd ${package}
+    mkdir build
+    cd build
+    mkdir ${install_path_header}/${install_path}
+    cmake ../ -DCMAKE_INSTALL_PREFIX=${install_path_header}/${install_path}
+    make
+    make install
+    cd ../..
 fi
 
-
 # install SuperLU-DIST
-CC=compiler_c
-CXX=compiler_cxx
-FC=compiler_fortran
+export CC=${compiler_mpicc}
 package=superlu_dist_5.3.0
 install_path=superlu_dist
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
     tar -xvf ${package}.gz
+    mv SuperLU_DIST_5.3.0 ${package}
     cd ${package}
-    ./configure --prefix=${install_path_header}/${install_path}
+    mkdir build
+    cd build
+    mkdir ${install_path_header}/${install_path}
+    cmake ../ \
+    -DCMAKE_C_FLAGS="-std=c99 -g" \
+    -Denable_blaslib=OFF \
+    -Denable_parmetislib=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_C_COMPILER=${compiler_mpicc} \
+    -DCMAKE_INSTALL_PREFIX=${install_path_header}/${install_path}
     make
     make install
-    cd ..
+    cd ../..
 fi
 
 
 # install PETSc
-CC=compiler_c
-FC=compiler_fortran
+CC=${compiler_c}
+FC=${compiler_fortran}
 package=petsc-3.8.3
 install_path=petsc
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
     tar -xvf ${package}.gz
     cd ${package}
     python2 ./configure --with-mpi-dir=${install_path_header}/mpich --download-fblaslapack --prefix=${install_path_header}/${install_path}
-    make PETSC_DIR=${install_path_header}/${package} PETSC_ARCH=arch-linux2-c-debug all
-    make PETSC_DIR=${install_path_header}/${package} PETSC_ARCH=arch-linux2-c-debug install
+    make PETSC_DIR=${source_codes_root_path}/${package} PETSC_ARCH=arch-linux2-c-debug all
+    make PETSC_DIR=${source_codes_root_path}/${package} PETSC_ARCH=arch-linux2-c-debug install
     cd ..
 fi
 
 
 # sundials
-CC=compiler_c
-FC=compiler_fortran
+CC=${compiler_c}
+FC=${compiler_fortran}
 package=sundials-3.1.0
 install_path=sundials
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
@@ -218,27 +261,33 @@ else
 
     make
     make install
-    rm -rf ${install_path_header}examples ${install_path_header}lib ${install_path_header}sundials-build
+    rm -rf ${install_path_header}/examples ${install_path_header}/lib ${install_path_header}/sundials-build
 fi
 
 
 
 # install umfpack included in SuiteSparse
-CC=compiler_c
-FC=compiler_fortran
-package=superlu_4.3
-install_path=SuperLU_4.3
+CC=${compiler_c}
+FC=${compiler_fortran}
+export CFLAGS="-fPIC"
+export CPPFLAGS="-fPIC"
+package=SuiteSparse-5.3.0
+install_path=SuiteSparse
 if [ -d ${install_path_header}/${install_path} ];then
     echo "${package} has been installed"
-    continue
 else
     tar -xvf ${package}.tar.gz
     tar -xvf ${package}.tar
     tar -xvf ${package}.gz
+    mv SuiteSparse ${package}
     cd ${package}
-    ./configure --prefix=${install_path_header}/${install_path}
-    make
-    make install
+    make BLAS=${install_path_header}/lapack/lib/libblas.a LAPACK=${install_path_header}/lapack/lib/liblapack.a
+    rm -rf ${install_path_header}/${install_path} 
+    mkdir ${install_path_header}/${install_path}
+    cp -r bin ${install_path_header}/${install_path}/bin
+    cp -r lib ${install_path_header}/${install_path}/lib
+    cp -r include ${install_path_header}/${install_path}/include
     cd ..
 fi
+
 
